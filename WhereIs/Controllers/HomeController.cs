61935a75238720b56bf917751b6355e48fdefa17
ali2 <ali62n62@yahoo.com>
2017-09-04 12:20:38 +0430

    just started

---
+++ WhereIs/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhereIs.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ViewResult Index()
+        {
+            int a = 10;
+            int b = 0;
+            double c = a / b;
+            return View();
+        }
+    }
+}
